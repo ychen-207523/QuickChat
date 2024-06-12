1921d9aec5d1c10ac98a81e7958dc0f4ff132648
Yunfei Chen <ychen207523@gmail.com>
2024-06-12 16:00:21 -0400

    add UserRepository and tests

---
+++ src/test/java/ychen/quickchat/repository/UserRepositoryTests.java
@@ -0,0 +1,45 @@
+package ychen.quickchat.repository;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import ychen.quickchat.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void whenFindByUsername_thenReturnUser() {
+        // Given
+        User alex = new User("JohnDoe", "Jdoe@example.com", "password@123");
+        entityManager.persist(alex);
+        entityManager.flush();
+
+        // When
+        User found = userRepository.findByUsername(alex.getUsername());
+
+        // Then
+        assertThat(found.getUsername()).isEqualTo(alex.getUsername());
+    }
+
+    @Test
+    public void whenInvalidUsername_thenReturnNull() {
+        // When
+        User fromDb = userRepository.findByUsername("doesNotExist");
+
+        // Then
+        assertThat(fromDb).isNull();
+    }
+}
