fef31f732338f6396216c68859ab932e0ba14e3b
Yunfei Chen <ychen207523@gmail.com>
2024-06-25 00:42:31 -0400

    update MessageRepository

--- src/main/java/ychen/quickchat/repository/MessageRepository.java
+++ src/main/java/ychen/quickchat/repository/MessageRepository.java
@@ -1,16 +1,22 @@
 package ychen.quickchat.repository;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.Query;
 import ychen.quickchat.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 @Repository
+@ComponentScan
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findBySenderId(Long senderId);
     List<Message> findByReceiverId(Long receiverId);
     List<Message> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
     List<Message> findAllByReceiverId(Long userId);
-    List<Message> findMessagesBetweenUsers(Long userId, Long senderId);
+    @Query("SELECT m FROM Message m WHERE (m.senderId = :senderId AND m.receiverId = :receiverId) OR (m.senderId = :receiverId AND m.receiverId = :senderId)")
+    List<Message> findMessagesBetweenUsers(@Param("senderId") Long senderId, @Param("receiverId") Long receiverId);
 }
