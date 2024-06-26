303428e8a49bca5096b18a686bf81813b7384a3e
Yunfei Chen <ychen207523@gmail.com>
2024-06-26 01:41:05 -0400

    add Authority class

---
+++ src/main/java/ychen/quickchat/model/Authority.java
@@ -0,0 +1,50 @@
+package ychen.quickchat.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authorities")
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String authority;
+
+    public Authority() {
+    }
+
+    public Authority(String username, String authority) {
+        this.username = username;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
