ce0bf1875f0e6286047cf7248fd17132777ca2fb
Yunfei Chen <ychen207523@gmail.com>
2024-07-15 23:20:27 -0400

    update user service

--- src/main/java/ychen/quickchat/service/UserService.java
+++ src/main/java/ychen/quickchat/service/UserService.java
@@ -1,6 +1,7 @@
 package ychen.quickchat.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ychen.quickchat.model.User;
 import ychen.quickchat.repository.UserRepository;
@@ -11,7 +12,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    public User signupUser(String username, String rawPassword) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(rawPassword));
+        user.setEnabled(true);
+        return userRepository.save(user);
     }
 }
