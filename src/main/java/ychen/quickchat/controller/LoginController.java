c8ab224ec8851c00f7f23fe8e1c30f66d27e2dd2
Yunfei Chen <ychen207523@gmail.com>
2024-07-29 22:52:32 -0400

    remove signup aip

--- src/main/java/ychen/quickchat/controller/LoginController.java
+++ src/main/java/ychen/quickchat/controller/LoginController.java
@@ -10,9 +10,4 @@ public class LoginController {
     public String login() {
         return "login";
     }
-
-    @GetMapping("/signup")
-    public String signup() {
-        return "signup";
-    }
 }
