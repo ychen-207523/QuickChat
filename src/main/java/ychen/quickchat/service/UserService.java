4feec2d2b588bc9e7a65fa4fcd80c56a1eb852d7
Yunfei Chen <ychen207523@gmail.com>
2024-07-29 23:03:05 -0400

    add save method

--- src/main/java/ychen/quickchat/service/UserService.java
+++ src/main/java/ychen/quickchat/service/UserService.java
@@ -26,4 +26,8 @@ public class UserService {
         user.setEnabled(true);
         return userRepository.save(user);
     }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
