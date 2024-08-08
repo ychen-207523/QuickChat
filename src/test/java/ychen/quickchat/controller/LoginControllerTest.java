5c94e984690175dc23a1e18b51cadfbe9ea9ae43
Yunfei Chen <ychen207523@gmail.com>
2024-08-08 01:14:53 -0400

    add LogControllerTest

---
+++ src/test/java/ychen/quickchat/controller/LoginControllerTest.java
@@ -0,0 +1,22 @@
+package ychen.quickchat.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LoginController.class)
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void loginPage() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk());
+    }
+}
